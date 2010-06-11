4d0032d57d0b326cfbc0a4a84164b1ae34afb2d3
Alex Yao Cheng <alex@thecellcity.com>
2010-06-11 16:15:29 +0800

    Atlas.h/m
    This is where all of the app's navigator routings will go. By placing the URL paths in a central location, we can easily change the URL routings without fear of breaking the app's navigation hierarchy.

---
+++ Classes/Atlas.m
@@ -0,0 +1,11 @@
+//
+//  Atlas.m
+//  SingtelDining
+//
+//  Created by Alex Yao on 6/11/10.
+//  Copyright 2010 CellCity. All rights reserved.
+//
+
+#import "Atlas.h"
+
+NSString* kAppRootURLPath = @"tt://root";
