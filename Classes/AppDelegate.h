5a2d99a021e2ba96c7461cc7b18c9f32045e1a4a
Alex Yao Cheng <alex@thecellcity.com>
2010-06-11 13:55:38 +0800

    init
    setup project with three20

---
+++ Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SingtelDiningAppDelegate.h
+//  SingtelDining
+//
+//  Created by Alex Yao on 6/11/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SingtelDining
+//
+//  Created by Alex Yao on 6/11/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+int main(int argc, char *argv[]) {
+  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+  int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+  [pool release];
+  return retVal;
+}
