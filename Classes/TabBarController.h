9f85d6bc0b8d56e0849a193b1a5e6745e1b8689a
Alex Yao Cheng <alex@thecellcity.com>
2010-06-14 18:40:57 +0800

    Tab bar
    customized tab bar

---
+++ Classes/TabBarController.h
@@ -0,0 +1,15 @@
+//
+//  TabBarController.h
+//  SingtelDining
+//
+//  Created by Alex Yao on 6/14/10.
+//  Copyright 2010 CellCity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TabBarController : UITabBarController {
+}
+
+@end
---
+++ Classes/TabBarController.m
@@ -0,0 +1,34 @@
+//
+//  TabBarController.m
+//  SingtelDining
+//
+//  Created by Alex Yao on 6/14/10.
+//  Copyright 2010 CellCity. All rights reserved.
+//
+
+#import "TabBarController.h"
+
+@interface UITabBarController (private)
+- (UITabBar *)tabBar;
+@end
+
+
+@implementation TabBarController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  CGRect frame = CGRectMake(0, 0, self.view.bounds.size.width, 49);
+  UIView *v = [[UIView alloc] initWithFrame:frame];
+  UIImage *i = [UIImage imageNamed:@"tabbar-bg.png"];
+  UIColor *c = [[UIColor alloc] initWithPatternImage:i];
+  v.backgroundColor = c;
+  [c release];
+  [[self tabBar] insertSubview:v atIndex:0];
+  [v release];
+  
+  [self setTabURLs:[NSArray arrayWithObjects:kAppLocaltionURLPath, kAppRestaurantsURLPath,
+                    kAppCuisinesURLPath, kAppFavouritesURLPath, kAPPSearchURLPath, nil]];
+}
+
+@end
