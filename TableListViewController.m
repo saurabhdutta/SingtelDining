cafe4d8d42d05fb3ad2d422ae25f73444c84047f
Alex Yao Cheng <alex@thecellcity.com>
2010-06-14 18:45:03 +0800

    Table list view
    show data in table list view. data source and model 
    not done yet

---
+++ TableListViewController.m
@@ -0,0 +1,34 @@
+//
+//  TableListViewController.m
+//  SingtelDining
+//
+//  Created by Alex Yao on 6/14/10.
+//  Copyright 2010 CellCity. All rights reserved.
+//
+
+#import "TableListViewController.h"
+
+
+@implementation TableListViewController
+
+- (id)initWithFrame:(CGRect)frame {
+  if (self = [super initWithFrame:frame]) {
+    self.dataSource = [TTListDataSource dataSourceWithItems:[NSArray arrayWithObjects:
+                                                             [TTTableSubtitleItem itemWithText:@"Aans Korea Resturants" subtitle:@"Orchard Central, #12-08" imageURL:@"bundle://sample-list-image.png" URL:kAppLocaltionURLPath],
+                                                             [TTTableSubtitleItem itemWithText:@"Aans Korea Resturants" subtitle:@"Orchard Central, #12-08" imageURL:@"bundle://sample-list-image.png" URL:kAppLocaltionURLPath],
+                                                             [TTTableSubtitleItem itemWithText:@"Aans Korea Resturants" subtitle:@"Orchard Central, #12-08" imageURL:@"bundle://sample-list-image.png" URL:kAppLocaltionURLPath],
+                                                             [TTTableSubtitleItem itemWithText:@"Aans Korea Resturants" subtitle:@"Orchard Central, #12-08" imageURL:@"bundle://sample-list-image.png" URL:kAppLocaltionURLPath],
+                                                             [TTTableSubtitleItem itemWithText:@"Aans Korea Resturants" subtitle:@"Orchard Central, #12-08" imageURL:@"bundle://sample-list-image.png" URL:kAppLocaltionURLPath],
+                                                             [TTTableSubtitleItem itemWithText:@"Aans Korea Resturants" subtitle:@"Orchard Central, #12-08" imageURL:@"bundle://sample-list-image.png" URL:kAppLocaltionURLPath],
+                                                             [TTTableSubtitleItem itemWithText:@"Aans Korea Resturants" subtitle:@"Orchard Central, #12-08" imageURL:@"bundle://sample-list-image.png" URL:kAppLocaltionURLPath],
+                                                             [TTTableSubtitleItem itemWithText:@"Aans Korea Resturants" subtitle:@"Orchard Central, #12-08" imageURL:@"bundle://sample-list-image.png" URL:kAppLocaltionURLPath],
+                                                             nil]];
+  }
+  return self;
+}
+
+- (void)dealloc {
+  [super dealloc];
+}
+
+@end
