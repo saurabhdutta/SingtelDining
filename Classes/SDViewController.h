1fa708bcc09305450d127f35f8ce70ec27ff3345
Alex Yao Cheng <alex@thecellcity.com>
2010-06-14 18:41:55 +0800

    parent view controller for views
    not yet

---
+++ Classes/SDViewController.h
@@ -0,0 +1,16 @@
+//
+//  SDViewController.h
+//  SingtelDining
+//
+//  Created by Alex Yao on 6/14/10.
+//  Copyright 2010 CellCity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SDViewController : TTViewController {
+
+}
+
+@end
---
+++ Classes/SDViewController.m
@@ -0,0 +1,21 @@
+//
+//  SDViewController.m
+//  SingtelDining
+//
+//  Created by Alex Yao on 6/14/10.
+//  Copyright 2010 CellCity. All rights reserved.
+//
+
+#import "SDViewController.h"
+
+
+@implementation SDViewController
+
+- (id)init {
+  if (self = [super init]) {
+    self.view.backgroundColor = [UIColor clearColor];
+  }
+  return self;
+}
+
+@end
