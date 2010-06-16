9f551410fda0d2921c018fff6da69935d51b5eb6
Alex Yao Cheng <alex@thecellcity.com>
2010-06-16 11:04:06 +0800

    two new controllers

---
+++ Classes/CuisinesViewController.m
@@ -0,0 +1,24 @@
+//
+//  CuisinesViewController.m
+//  SingtelDining
+//
+//  Created by Alex Yao on 6/16/10.
+//  Copyright 2010 CellCity. All rights reserved.
+//
+
+#import "CuisinesViewController.h"
+#import "SDListView.h"
+
+
+@implementation CuisinesViewController
+
+#pragma mark -
+- (void)loadView {
+  [super loadView];
+  
+  SDListView *boxView = [[SDListView alloc] initWithFrame:CGRectMake(5, 0, 310, 300)];
+  [self.view addSubview:boxView];
+  [boxView release];
+}
+
+@end
