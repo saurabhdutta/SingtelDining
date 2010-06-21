04fcf016ef9faa1c42055910258ccf0c81ab677a
Alex Yao Cheng <alex@thecellcity.com>
2010-06-21 15:25:19 +0800

    back button on the navigationbar

--- Classes/SDViewController.m
+++ Classes/SDViewController.m
@@ -11,10 +11,31 @@
 
 @implementation SDViewController
 
+- (id)init {
+  if (self = [super init]) {
+    //self.title = @"test";
+    self.navigationItem.backBarButtonItem = [[[UIBarButtonItem alloc] initWithTitle:@"HOME" 
+                                                                              style:UIBarButtonItemStyleBordered 
+                                                                             target:nil 
+                                                                             action:nil] 
+                                             autorelease];
+    self.navigationItem.hidesBackButton = YES;
+  }
+  return self;
+}
+
 - (void)loadView {
   [super loadView];
   self.view.backgroundColor = [UIColor clearColor];
   self.navigationController.navigationBar.backgroundColor = [UIColor clearColor];
+  
+  UIButton *doneButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 57, 30)];
+  [doneButton setImage:[UIImage imageNamed:@"button-done.png"] forState:UIControlStateNormal];
+  [doneButton addTarget:self action:@selector(doneButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+  UIBarButtonItem *barDoneButton = [[UIBarButtonItem alloc] initWithCustomView:doneButton];
+  [doneButton release];
+  self.navigationController.navigationItem.backBarButtonItem = barDoneButton;
+  [barDoneButton release];
   
 }
 
