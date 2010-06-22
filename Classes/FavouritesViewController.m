0ae57fda970c7bd30e8bd86f7049d608f99cc6ad
Alex Yao Cheng <alex@thecellcity.com>
2010-06-22 23:31:10 +0800

    toggle table editable on favorite view

--- Classes/FavouritesViewController.m
+++ Classes/FavouritesViewController.m
@@ -13,28 +13,8 @@
 
 @implementation FavouritesViewController
 
-- (IBAction)editButtonClicked:(id)sender {
-  [self.tableView setEditing:YES animated:YES];
-  
-  UIBarButtonItem *editButton = self.navigationItem.rightBarButtonItem;
-  [editButton setTitle:@""];
-  [editButton setImage:[UIImage imageNamed:@"button-done.png"]];
-  [editButton setAction:@selector(cancelButtonClicked:)];
-}
-- (IBAction)cancelButtonClicked:(id)sender {
-  [self.tableView setEditing:NO animated:YES];
-  
-  UIBarButtonItem *editButton = self.navigationItem.rightBarButtonItem;
-  [editButton setTitle:@"Edit"];
-  [editButton setAction:@selector(editButtonClicked:)];
-}
-
 - (void)loadView {
   [super loadView];
-  
-  UIBarButtonItem *editButton = [[UIBarButtonItem alloc] initWithTitle:@"Edit" style:UIBarButtonItemStyleBordered target:self action:@selector(editButtonClicked:)];
-  self.navigationItem.rightBarButtonItem = editButton;
-  TT_RELEASE_SAFELY(editButton);
   
   UIImageView *titleView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 128, 19)];
   titleView.image = [UIImage imageNamed:@"credit-title.png"];
@@ -44,6 +24,7 @@
     self.tableView.frame = CGRectMake(5, 40, 300, kBoxNormalHeight-50);
     self.tableView.backgroundColor = [UIColor clearColor];
     [boxView addSubview:self.tableView];
+    self.tableView.editing = YES;
   }
   [self.view addSubview:boxView];
   [boxView release];
